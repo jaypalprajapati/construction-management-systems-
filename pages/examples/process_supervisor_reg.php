45e61bf1a592052a9b7ea75b44d5a8d33d8947a9
Jaypal Prajapati <85919639+jaypalprajapati@users.noreply.github.com>
2023-05-10 15:09:15 +0530

    Update process_supervisor_reg.php

--- pages/examples/process_supervisor_reg.php
+++ pages/examples/process_supervisor_reg.php
@@ -76,8 +76,8 @@ if ($rs)
 	$mail->isSMTP();                                   // Set mailer to use SMTP
 	$mail->Host = 'smtp.gmail.com';                    // Specify main and backup SMTP servers
 	$mail->SMTPAuth = true;                            // Enable SMTP authentication
-	$mail->Username = 'devconstruction1411@gmail.com';          // SMTP username
-	$mail->Password = 'dev8733991144'; // SMTP password
+	$mail->Username = 'dev@gmail.com';          // SMTP username
+	$mail->Password = 'dev4'; // SMTP password
 	$mail->SMTPSecure = 'tls';                         // Enable TLS encryption, `ssl` also accepted
 	$mail->Port = 587;                                 // TCP port to connect to
 
